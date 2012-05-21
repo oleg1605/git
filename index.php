7c730eb6635c96f24ecdf35facce39c7fe1d9330
oleg1605 <oleg1605@mail.ru>
2012-05-21 13:19:45 +0300

    test 8
    Signed-off-by: oleg1605 <oleg1605@mail.ru>

--- index.php
+++ index.php
@@ -7,5 +7,11 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 // remove the following line when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
 
+
+
+
+//
+
+
 require_once($yii);
 Yii::createWebApplication($config)->run();
